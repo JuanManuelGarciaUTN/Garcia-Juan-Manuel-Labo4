f4946ca144b1ea9702059e1a7f7a3cfd91a6c4fb
JuanManuelGarciaUTN <artlixUTN@gmail.com>
2023-03-30 22:00:30 -0300

    inicializo repositorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primera-app-angular';
+  title = 'Primera-App-Angular-UTN';
+  tituloNuevo = "";
+  numeroUno:number = 0;
+  numeroDos:number = 0;
+  resultado:number = 0;
+
+  cambiarTitutlo(){
+    this.title = this.tituloNuevo;
+    this.tituloNuevo = "";
+  };
+
+  sumar(){
+    //this.resultado = Number(this.numeroUno) + Number(this.numeroDos);
+    this.resultado = this.numeroUno + this.numeroDos;
+  }
 }
